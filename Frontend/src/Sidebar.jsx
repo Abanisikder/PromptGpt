865c6b859ad3cf1c479585d1fe605203c63f80e5
Abani Sikder <abanisikder312@gmail.com>
2026-04-09 00:45:47 +0600

    adding the css of sidebar component

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,15 +1,28 @@
 
 import React from 'react'
 import "./Sidebar.css"
+import logo from "./assets/promptgpt.png";
 
 function Sidebar() {
   return (
     <section className='sidebar'>
       <button>
-        <img src="src/assets/promptgpt.png" alt="promptgpt logo"></img>
-        <i class="fa-regular fa-pen-to-square"></i>
+       <img src={logo} alt="promptgpt logo" className='logo' />
+       <span> <i className="fa-regular fa-pen-to-square"></i></span>
       </button>
-      
+      <div className='history'>
+      <ul >
+        <li>history 1</li>
+        <li>history 2</li>
+        <li>history 3</li>
+        <li>history 4</li>
+      </ul>
+      </div>
+      <div className='sign'>
+        <p>Owner Abani Sikder &hearts;</p>
+
+      </div>
+
 
     </section>
   )
