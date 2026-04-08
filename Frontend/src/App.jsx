4bab391497385d541aa2b7c8bfbd604538f65346
Abani Sikder <abanisikder312@gmail.com>
2026-04-08 23:44:29 +0600

    creating context component and implement some style in app component like chatgpt

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,16 +2,20 @@
 import './App.css'
 import Sidebar from './sidebar'
 import Chatwindow from './Chatwindow'
+import { MyContext } from './MyContext'
 
 function App() {
+  const providerValues={};
   
 
   return (
-    <>
+    <div className='app'>
+      <MyContext.Provider value={providerValues}>
     <Sidebar/>
     <Chatwindow/>
+    </MyContext.Provider>
       
-    </>
+    </div>
   )
 }
 
