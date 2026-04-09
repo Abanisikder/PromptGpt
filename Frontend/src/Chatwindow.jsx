9686e8ae0835c2edf8826ec54b046cb1593e8db9
Abani Sikder <abanisikder312@gmail.com>
2026-04-09 22:32:19 +0600

    creating chatwindow component

--- Frontend/src/Chatwindow.jsx
+++ Frontend/src/Chatwindow.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./Chatwindow.css"
+import React from "react";
+import "./Chatwindow.css";
+import Chat from "./Chat.jsx"
 
 function Chatwindow() {
   return (
-    <div>Chatwindow</div>
-  )
+    <div className="chatwindow">
+      <div className="navbar">
+        <span>
+          PromtGpt &nbsp;<i className="fa-solid fa-angle-down"></i>
+        </span>
+        <div className="user">
+          <span>
+            <i className="fa-solid fa-user"></i>
+          </span>
+        </div>
+      </div>
+      <Chat/>
+      <div className="inputdiv">
+        <input></input>
+        <div className="info">
+          <p>PromptGpt can make mistakes. Check important info.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Chatwindow
+export default Chatwindow;
